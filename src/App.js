7adc82d85c4d46ac0ddb17abe421e1573800f610
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-01 16:08:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Add, Send } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Typography variant="h6" m={3}>
+        Button Types
+      </Typography>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant="h6" m={3}>
+        Disabled Button
+      </Typography>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h6" m={3}>
+        Secondary Color Button and small button
+      </Typography>
+      <Button variant="contained" color="secondary">
+        Contained
+      </Button>
+      <Button variant="contained" size="small">
+        Contained
+      </Button>
+      <Typography variant="h6" m={3}>
+        Buttons with Icons
+      </Typography>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<SettingsIcon />}
+      >
+        Settings
+      </Button>
+      <Button variant="contained" color="success" startIcon={<Add />}>
+        Add Post
+      </Button>
+      <Button variant="contained" color="warning" endIcon={<Send />}>
+        Send Mail
+      </Button>
+      <Typography variant="h1" component="p">
+        it uses h1 style but it is a p tag
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: 'pink',
+          color: '#f00',
+          margin: 5,
+          '&:hover': {
+            backgroundColor: 'peachpuff',
+            color: '#0f0',
+          },
+        }}
+      >
+        My Unique Button
+      </Button>
+      <Button
+        variant="contained"
+        disabled
+        sx={{
+          backgroundColor: 'pink',
+          color: '#f00',
+          margin: 5,
+          '&:hover': {
+            backgroundColor: 'peachpuff',
+            color: '#0f0',
+          },
+          '&:disabled': {
+            backgroundColor: 'darkgray',
+            color: 'white',
+          },
+        }}
+      >
+        My Unique Button - Disabled
+      </Button>
+    </main>
   );
 }
 
